f277672fe1a6e636946a334d674f5cba1fc6ebb6
Toni Blåfield <toni.blafield@tuni.fi>
2020-01-23 00:03:18 +0200

    tuned startup bat

--- start.bat
+++ start.bat
@@ -21,20 +21,20 @@ title %serverName% batch
 ::DayZServer location (DONT edit)
 cd "%serverLocation%"
 
-:start
+:launch
 
 taskkill /im DayZServer_x64.exe /F
 
-::Time in seconds to wait before..
-timeout 5
+::Time in seconds to wait before starting
+timeout 3
 
 echo (%time%) %serverName% started.
 
 ::Launch parameters (edit end: -config=|-port=|-profiles=|-doLogs|-adminLog|-netLog|-freezeCheck|-filePatching|-BEpath=|-cpuCount=)
 start "DayZ_Server" "DayZServer_x64.exe" -config=%serverConfig% -port=%serverPort% -cpuCount=%serverCPU% -dologs -adminlog -netlog -freezecheck
 
-::Time in seconds before kill server process (14400 = 4 hours)
-timeout 84600
+::Time in seconds before kill server process time + 40 to match server shutdown time
+timeout 86440
 
 ::Go back to the top and repeat the whole cycle again
-goto start
+goto launch
